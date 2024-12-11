38557e603b6f8834456fff89ebfb6c7cd2b5fae6
Leshan21 <leshanpasindu12@gmail.com>
2024-12-11 15:54:56 +0530

    some changes

---
+++ basic-module/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                    <li>Address</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
